cf1c7269f3964992c044a44c2fe525a13af26923
Brandon Donham <bdonham@bioinf1.indstate.edu>
2015-08-27 16:49:54 -0400

    added argument check

--- readlength.sh
+++ readlength.sh
@@ -3,4 +3,8 @@
 BASEDIR=$(dirname $(realpath $0))
 source "${BASEDIR}/rsr_config.sh"
 
-echo $(( $(head -2 $1 | tail -1 | wc -c) - $OFFSET ))
+if (( $# < 1 )); then
+    die "usage  -- $0 <reads file>"
+else
+    echo $(( $(head -2 $1 | tail -1 | wc -c) - $OFFSET ))
+fi
